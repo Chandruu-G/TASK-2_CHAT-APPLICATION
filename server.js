5e15f1923c9d26bb161917f40fe40991b7bbc0bc
chandruu <chandruvpl2006@gmail.com>
2025-08-04 23:02:10 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from "public" folder
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // Broadcast to all users
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
